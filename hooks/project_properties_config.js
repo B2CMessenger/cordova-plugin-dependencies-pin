51b6827c2c7033f25b41477ea6b13b6616f59f13
hardcor4uk <hardcorka@gmail.com>
2019-09-17 16:40:38 +0400

    fix android-support-version. bump version

--- hooks/project_properties_config.js
+++ hooks/project_properties_config.js
@@ -3,8 +3,8 @@ module.exports = {
     "com.google.android.gms:play-services-analytics": "+",
     "com.google.android.gms:play-services-auth": "+",
     "com.google.android.gms:play-services-identity": "+",
-    "com.android.support:support-v4": "27.1.0",
-    "com.android.support:support-v13": "27.1.0",
+    "com.android.support:support-v4": "27.+",
+    "com.android.support:support-v13": "27.+",
     "me.leolin:ShortcutBadger": "1.1.17@aar",
     "com.google.firebase:firebase-messaging": "+"
 };
