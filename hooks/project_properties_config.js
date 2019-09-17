5a3759d2b0e1739d2253cca7feb9f8a7897186a0
hardcor4uk <hardcorka@gmail.com>
2019-09-17 16:13:48 +0400

    fix google-play-services. bump version.

--- hooks/project_properties_config.js
+++ hooks/project_properties_config.js
@@ -1,10 +1,10 @@
 module.exports = {
     "com.facebook.android:facebook-android-sdk": "4.25.+",
-    "com.google.android.gms:play-services-analytics": "11.8.0",
-    "com.google.android.gms:play-services-auth": "11.8.0",
-    "com.google.android.gms:play-services-identity": "11.8.0",
+    "com.google.android.gms:play-services-analytics": "+",
+    "com.google.android.gms:play-services-auth": "+",
+    "com.google.android.gms:play-services-identity": "+",
     "com.android.support:support-v4": "27.1.0",
     "com.android.support:support-v13": "27.1.0",
     "me.leolin:ShortcutBadger": "1.1.17@aar",
-    "com.google.firebase:firebase-messaging": "11.8.0"
+    "com.google.firebase:firebase-messaging": "+"
 };
